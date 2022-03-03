982553e6a33264094d6bc097650bae62439dfe2f
saral98 <sarl98@mi.fu-berlin.de>
2022-03-03 15:25:32 +0100

    changes is genomic_distances and computecrup

--- R/compute_crup_ep.R
+++ R/compute_crup_ep.R
@@ -2,22 +2,20 @@
 # function: get enhancer scores for enhancers
 ###############################################################################
 compute_crup_EP_enhancer <- function(input, crup_scores) {
-
+  print("Computing CRUP EP scores for enhancer...")
   ccres_enhancer <- as.data.frame(ccres_enhancer)
 
-  regions <- subset(ccres_enhancer, ccres_enhancer$V5 %in% input$V2,
-                    select = c(V1, V5, new_start, new_end))
+  regions <- subset(ccres_enhancer, ccres_enhancer$V5 %in% input$V2, select = c(V1, V5, new_start, new_end))
 
-  genes_ranges <- with(regions, GenomicRanges::GRanges(
-    V1, IRanges::IRanges(start = new_start, end = new_end)))
+  genes_ranges <- with(regions, GenomicRanges::GRanges(V1, IRanges::IRanges(start = new_start, end = new_end)))
 
   hits_crup <- GenomicRanges::findOverlaps(genes_ranges, crup_scores)
   cres_EP <- data.frame(cres = hits_crup@from, EP = hits_crup@to)
   cres_EP$cres_name <- ccres_enhancer$V5[cres_EP$cres]
-  cres_EP$EP_prob <- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
-  normilized_score <- aggregate(EP_prob ~ cres_name, cres_EP, sum)
-
-  return(normilized_score)
+  cres_EP$EP_enhancer <- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
+  cres_EP$bin<-rep(1:5,nrow(regions))
+  trial<-reshape(cres_EP,direction="wide",timevar = "bin",idvar="cres_name",v.names="EP_enhancer")
+  return(trial[,4:8])
 
 
 
@@ -27,22 +25,21 @@ compute_crup_EP_enhancer <- function(input, crup_scores) {
 # function: get enhancer scores for promoters
 ###############################################################################
 compute_crup_EP_promoter <- function(input, crup_scores) {
-
+  print("Computing CRUP EP scores for promoter...")
   gencode <- as.data.frame(gencode)
 
   regions <- subset(gencode, gencode$gene_id %in% input[, 1],
                     select = c(chr, gene_id, new_start, new_end))
 
-  genes_ranges <- with(regions, GenomicRanges::GRanges(
-    chr, IRanges::IRanges(start = new_start, end = new_end)))
+  genes_ranges <- with(regions, GenomicRanges::GRanges(chr, IRanges::IRanges(start = new_start, end = new_end)))
 
   hits_crup <- GenomicRanges::findOverlaps(genes_ranges, crup_scores)
   cres_EP <- data.frame(promoter = hits_crup@from, EP = hits_crup@to)
   cres_EP$gene_name <- gencode$gene_id[cres_EP$promoter]
-  cres_EP$EP_prob <- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
-  normilized_score  <- aggregate(EP_prob ~ gene_name, cres_EP, sum)
-
-  return(normilized_score)
+  cres_EP$EP_promoter <- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
+  cres_EP$bin<-rep(1:5,nrow(regions))
+  trial<-reshape(cres_EP,idvar="gene_name",timevar = "bin",direction="wide",v.names="EP_promoter")
+  return(trial[,4:8])
 
 
 }
@@ -52,6 +49,7 @@ compute_crup_EP_promoter <- function(input, crup_scores) {
 ###############################################################################
 
 compute_crup_EP_reg_distance <- function(input, prediction) {
+  print("Computing CRUP EP scores for regulatory distance...")
   ccres_enhancer <- unfactorize(ccres_enhancer)
   gencode <- unfactorize(gencode)
   middle_point <- ccres_enhancer[ccres_enhancer$V5 %in% input$V2, ]$middle_point
@@ -68,12 +66,9 @@ compute_crup_EP_reg_distance <- function(input, prediction) {
 
   input$pair <- paste0(input$V1, "_", input$V2)#173509
   input <- input[!(duplicated(input$pair)), ]#169049
-
-  between_ranges <- with(input, GenomicRanges::GRanges(
-    chr, IRanges::IRanges(start = bstart, end = bend)))
+  between_ranges <- with(input, GenomicRanges::GRanges(chr, IRanges::IRanges(start = bstart, end = bend)))
   hits_enh <- GenomicRanges::findOverlaps(between_ranges, prediction)
-  cres_EP <- data.frame(between = hits_enh@from,
-                      EP_prob = GenomicRanges::elementMetadata(prediction)$score[hits_enh@to])
+  cres_EP <- data.frame(between = hits_enh@from, EP_prob = GenomicRanges::elementMetadata(prediction)$score[hits_enh@to])
   normilized_score <- aggregate(EP_prob ~ between, cres_EP, mean)
   return(normilized_score)
 
@@ -84,23 +79,21 @@ compute_crup_EP_reg_distance <- function(input, prediction) {
 # function: get promoter scores for enhancer
 ###############################################################################
 compute_crup_PP_enhancer <- function(input, crup_scores) {
-
+  print("Computing CRUP PP scores for enhancer...")
   ccres_enhancer <- as.data.frame(ccres_enhancer)
 
   regions <- subset(ccres_enhancer, ccres_enhancer$V5 %in% input$V2,
                     select = c(V1, V5, new_start, new_end))
 
-  genes_ranges <- with(regions, GenomicRanges::GRanges(
-    V1, IRanges::IRanges(start = new_start, end = new_end)))
+  genes_ranges <- with(regions, GenomicRanges::GRanges(V1, IRanges::IRanges(start = new_start, end = new_end)))
 
   hits_crup <- GenomicRanges::findOverlaps(genes_ranges, crup_scores)
   cres_EP <- data.frame(cres = hits_crup@from, EP = hits_crup@to)
   cres_EP$cres_name <- ccres_enhancer$V5[cres_EP$cres]
-  cres_EP$EP_prob <- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
-  normilized_score <- aggregate(EP_prob ~ cres_name, cres_EP, sum)
-
-  return(normilized_score)
-
+  cres_EP$PP_enhancer <- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
+  cres_EP$bin<-rep(1:5,nrow(regions))
+  trial<-reshape(cres_EP,idvar="cres_name",timevar = "bin",direction="wide",v.names="PP_enhancer")
+  return(trial[,4:8])
 
 
 }
@@ -110,22 +103,21 @@ compute_crup_PP_enhancer <- function(input, crup_scores) {
 ###############################################################################
 
 compute_crup_PP_promoter <- function(input, crup_scores) {
+  print("Computing CRUP PP scores for promoter...")
   gencode <- as.data.frame(gencode)
 
   regions <- subset(gencode, gencode$gene_id %in% input[, 1],
                     select = c(chr, gene_id, new_start, new_end))
 
-  genes_ranges <- with(regions, GenomicRanges::GRanges(
-    chr, IRanges::IRanges(start = new_start, end = new_end)))
+  genes_ranges <- with(regions, GenomicRanges::GRanges(chr, IRanges::IRanges(start = new_start, end = new_end)))
 
   hits_crup <- GenomicRanges::findOverlaps(genes_ranges, crup_scores)
   cres_EP <- data.frame(promoter = hits_crup@from, EP = hits_crup@to)
   cres_EP$gene_name <- gencode$gene_id[cres_EP$promoter]
-  cres_EP$EP_prob <- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
-  normilized_score <- aggregate(EP_prob ~ gene_name, cres_EP, sum)
-
-  return(normilized_score)
-
+  cres_EP$PP_promoter<- GenomicRanges::elementMetadata(crup_scores)$score[cres_EP$EP]
+  cres_EP$bin<-rep(1:5,nrow(regions))
+  trial<-reshape(cres_EP,idvar="gene_name",timevar = "bin",direction="wide",v.names="PP_promoter")
+  return(trial[,4:8])
 
 }
 
@@ -133,6 +125,7 @@ compute_crup_PP_promoter <- function(input, crup_scores) {
 # function: get promoter scores for regulatory distance
 ###############################################################################
 compute_crup_PP_reg_distance <- function(input, prediction) {
+  print("Computing CRUP PP scores for regulatory distance...")
   ccres_enhancer <- unfactorize(ccres_enhancer)
   gencode <- unfactorize(gencode)
   middle_point <- ccres_enhancer[ccres_enhancer$V5 %in% input$V2, ]$middle_point
@@ -150,8 +143,7 @@ compute_crup_PP_reg_distance <- function(input, prediction) {
   input$pair <- paste0(input$V1, "_", input$V2)#173509
   input <- input[!(duplicated(input$pair)), ]#169049
 
-  between_ranges <- with(input, GenomicRanges::GRanges(
-    chr,IRanges::IRanges(start=bstart, end=bend)))
+  between_ranges <- with(input, GenomicRanges::GRanges(chr,IRanges::IRanges(start=bstart, end=bend)))
   hits_enh <- GenomicRanges::findOverlaps(between_ranges, prediction)
 
   cres_EP <- data.frame(between = hits_enh@from,
@@ -185,12 +177,12 @@ compute_features_crup_scores <- function(input, crupinput, cores, chromosome){
   crup_PP_prom <- compute_crup_PP_promoter(input, crup_scores_prom)
   crup_PP_reg <- compute_crup_PP_reg_distance(input, crup_scores_prom)
 
-  input$EP_enh <- crup_EP_enh$EP_prob
-  input$EP_prom <- crup_EP_prom$EP_prob
-  input$EP_distance <- crup_EP_reg$EP_prob
-  input$PP_enh <- crup_PP_enh$EP_prob
-  input$PP_prom <- crup_PP_prom$EP_prob
-  input$PP_distance <- crup_PP_reg$EP_prob
+  input <- cbind(input, crup_EP_enh)
+  input <- cbind(input, crup_EP_prom)
+  input <- cbind(input, crup_EP_reg)
+  input <- cbind(input, crup_PP_enh)
+  input <- cbind(input, crup_PP_prom)
+  input <- cbind(input, crup_PP_reg)
 
   return(input)
 
--- R/genomic_distances.R
+++ R/genomic_distances.R
@@ -1,19 +1,83 @@
+###############################################################################
+# function: convert factor type columns into character type
+###############################################################################
+unfactorize <- function(df) {
+  for (i in which(sapply(df, class) == "factor")) {
+    df[[i]] <- as.character(df[[i]])
+  }
 
-#' Compute genomic distances
-#'
-#' @param x A dataframe of either two columns (gene and enhancer pairs) or
-#' one colum of genes
-#'
-#'
-#' @return A dataframe with the gene and enhancer pairs and the genomic distance
-#' between them
-#'
-#' @export
-#'
-#' @import GenomicRanges
-#' @examples
-#'
-#'
+  return(df)
+}
+
+###############################################################################
+# function: get the distance from gene to enhancer
+###############################################################################
+distances_gene_enhancer <- function(x) {
+  x <- unfactorize(x)
+  gencode <- unfactorize(gencode)
+  x$chr_gene <- gencode[gencode$gene_id %in% x[, 1], 1]
+  x$chr_enh <- ccres_enhancer[ccres_enhancer$V5 %in% x[,2],1]
+  x$gene_tts <- gencode[gencode$gene_id %in% x[, 1], 10]
+  x$enh_middlepoint <- ccres_enhancer[ccres_enhancer$V5 %in% x[, 2], 11]
+
+  for(i in 1:nrow(x)){
+    if(x[i,]$chr_gene != x[i,]$chr_enh){
+      warning("Gene and enhancer are not in the same chromosome,
+            distance will not be computed")
+      x <- x[- i,] #what happens if all are in diff chromosomes
+    }
+  }
+
+  x$distance <- abs(x$enh_middlepoint - x$gene_tts)
+
+  return(x)
+}
+
+###############################################################################
+# function: extend genes by 50000bp and map the gene region
+# to overlapping enhancers
+###############################################################################
+
+
+map_genes_to_chromosomes <- function(x){
+  x <- unfactorize(x)
+  #get chromosome and tts of our genes
+  x$chr_gene <- gencode[gencode$gene_id %in% x[, 1], 1]
+  x$gene_tts <- gencode[gencode$gene_id %in% x[, 1], 10]
+
+  #extend start coorditantes of tts
+  x$start_tts <- x$gene_tts
+  x$start_tts[x$gene_tts <= 50000] <- 1
+  x$start_tts[x$gene_tts > 50000] <- x$start_tts[ x$gene_tts > 50000] - 50000
+
+  #extend end coordinates of tts
+  x$end_tts <- x$gene_tts
+  x$end_tts[x$gene_tts + 50000 > chromosomes[chromosomes[, 1] %in% x$chr_gene, 2]] <- chromosomes[chromosomes[, 1] %in% x$chr_gene, 2]
+  x$end_tts[x$gene_tts + 50000 <= chromosomes[chromosomes[, 1] %in% x$chr_gene, 2]] <-
+    x$end_tts[x$gene_tts + 50000 <= chromosomes[chromosomes[, 1] %in% x$chr_gene, 2]] + 50000
+
+
+  genes_range <- with(x, GenomicRanges::GRanges(chr_gene, IRanges::IRanges(start_tts, end_tts)))
+
+  enhancer_range<-  with(ccres_enhancer, GenomicRanges::GRanges(V1, IRanges::IRanges(start=new_start,end=new_end)))
+
+  overlaps <- GenomicRanges::findOverlaps(genes_range, enhancer_range,
+                                          ignore.strand = T)
+
+  cres_overlaping <-data.frame(gene=overlaps@from,enhancer=overlaps@to)
+  cres_overlaping$gene_id <- x[cres_overlaping$gene,1]
+  cres_overlaping$enhancer_id <- ccres_enhancer$V5[cres_overlaping$enhancer]
+  cres_overlaping <- cres_overlaping[,3:4]
+  return(cres_overlaping)
+
+
+}
+
+###############################################################################
+# function: compute the distances between genes and enhancers
+###############################################################################
+
+
 distances <- function(x) {
   x <- unfactorize(x)
   gencode <- as.data.frame(gencode)
@@ -26,6 +90,7 @@ distances <- function(x) {
   else{
 
     returned_mat <- map_genes_to_chromosomes(x)
+    print(returned_mat)
     distance_dataframe <- distances_gene_enhancer(returned_mat)
     return(distance_dataframe)
 
